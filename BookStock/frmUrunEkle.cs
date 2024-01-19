24250840602b7f844b6b491815d42cdb2235a5eb
olyadev <yusupowa9494@mail.ru>
2024-01-19 19:42:53 +0300

    Kategori Ve Yazar Butonları Silindi

--- BookStock/frmUrunEkle.cs
+++ BookStock/frmUrunEkle.cs
@@ -37,34 +37,34 @@ namespace BookStock
         {
         }
 
-        private void KategoriGetir()
-        {
-            connection.Open();
-            SqlCommand cmd = new SqlCommand("select *from KategoriBilgileri", connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                comboKategori.Items.Add(reader["kategori"].ToString());
-            }
-            connection.Close();
-        }
+        //private void KategoriGetir()
+        //{
+        //    connection.Open();
+        //    SqlCommand cmd = new SqlCommand("select *from KategoriBilgileri", connection);
+        //    SqlDataReader reader = cmd.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        comboKategori.Items.Add(reader["kategori"].ToString());
+        //    }
+        //    connection.Close();
+        //}
         private void frmUrunEkle_Load(object sender, EventArgs e)
         {
-            KategoriGetir();
+            //KategoriGetir();
         }
 
         private void comboKategori_SelectedIndexChanged(object sender, EventArgs e)
         {
-            comboYazar.Items.Clear();
-            comboYazar.Text = "";
-            connection.Open();
-            SqlCommand cmd = new SqlCommand("select * from MarkaBilgileri where kategori ='" + comboKategori.SelectedItem + "'", connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                comboKategori.Items.Add(reader["marka"].ToString());
-            }
-            connection.Close();
+            //comboYazar.Items.Clear();
+            //comboYazar.Text = "";
+            //connection.Open();
+            //SqlCommand cmd = new SqlCommand("select * from MarkaBilgileri where kategori ='" + comboKategori.SelectedItem + "'", connection);
+            //SqlDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    comboKategori.Items.Add(reader["marka"].ToString());
+            //}
+            //connection.Close();
         }
 
         private void btnYeniEkle_Click(object sender, EventArgs e)
@@ -73,10 +73,10 @@ namespace BookStock
             if (durum == true)
             {
                 connection.Open();
-                SqlCommand cmd = new SqlCommand("insert into Urun(barkodno,kategori,marka,urunadi,miktari,alisfiyati,satisfiyati,tarih) values (@barkodno,@kategori,@marka,@urunadi,@miktari,@alisfiyati,@satisfiyati,@tarih)", connection);
+                SqlCommand cmd = new SqlCommand("insert into Urun(barkodno,urunadi,miktari,alisfiyati,satisfiyati,tarih) values (@barkodno,@urunadi,@miktari,@alisfiyati,@satisfiyati,@tarih)", connection);
                 cmd.Parameters.AddWithValue("@barkodno", txtBarkodNo.Text);
-                cmd.Parameters.AddWithValue("@kategori", comboKategori.Text);
-                cmd.Parameters.AddWithValue("@marka", comboYazar.Text);
+                //cmd.Parameters.AddWithValue("@kategori", comboKategori.Text);
+                //cmd.Parameters.AddWithValue("@marka", comboYazar.Text);
                 cmd.Parameters.AddWithValue("@urunadi", txtUrunAdi.Text);
                 cmd.Parameters.AddWithValue("@miktari", int.Parse(txtMiktari.Text));
                 cmd.Parameters.AddWithValue("@alisfiyati", double.Parse(txtAlisFiyat.Text));
@@ -93,7 +93,7 @@ namespace BookStock
                 MessageBox.Show("Böyle bir BakodNo var zaten veya buton boş geçilemez", "Uyarı!");
             }
 
-            comboYazar.Items.Clear();
+            //comboYazar.Items.Clear();
             foreach (Control item in groupBox1.Controls)
             {
                 if (item is System.Windows.Forms.TextBox)
@@ -109,48 +109,48 @@ namespace BookStock
 
         private void BarkodNoTxt_TextChanged(object sender, EventArgs e)
         {
-            if (BarkodNoTxt.Text == "")
-            {
-                lblMiktar.Text = "";
-                foreach (Control item in groupBox2.Controls)
-                {
-                    if (item is System.Windows.Forms.TextBox)
-                    {
-                        item.Text = "";
-                    }
-                }
-            }
-            connection.Open();
-            SqlCommand cmd = new SqlCommand("select * from Urun where barkodno like'" + BarkodNoTxt.Text + "%'", connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read()) //kayıtlar okunduğu sürece döngü devam edecek
-            {
-                KategoriTxt.Text = reader["kategori"].ToString();
-                YazarTxt.Text = reader["marka"].ToString();
-                UrunAdiTxt.Text = reader["urunadi"].ToString();
-                lblMiktar.Text = reader["miktari"].ToString();
-                AlisFiyatTxt.Text = reader["alisfiyati"].ToString();
-                SatisFiyatTxt.Text = reader["satisfiyati"].ToString();
-            }
-            connection.Close();
+            //if (BarkodNoTxt.Text == "")
+            //{
+            //    lblMiktar.Text = "";
+            //    foreach (Control item in groupBox2.Controls)
+            //    {
+            //        if (item is System.Windows.Forms.TextBox)
+            //        {
+            //            item.Text = "";
+            //        }
+            //    }
+            //}
+            //connection.Open();
+            ////SqlCommand cmd = new SqlCommand("select * from Urun where barkodno like'" + BarkodNoTxt.Text + "%'", connection);
+            //SqlDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read()) //kayıtlar okunduğu sürece döngü devam edecek
+            //{
+            //    //KategoriTxt.Text = reader["kategori"].ToString();
+            //    //YazarTxt.Text = reader["marka"].ToString();
+            //    UrunAdiTxt.Text = reader["urunadi"].ToString();
+            //    lblMiktar.Text = reader["miktari"].ToString();
+            //    AlisFiyatTxt.Text = reader["alisfiyati"].ToString();
+            //    SatisFiyatTxt.Text = reader["satisfiyati"].ToString();
+            //}
+            //connection.Close();
 
         }
 
         private void btnVarOlanaEkle_Click(object sender, EventArgs e)
         {
-            connection.Open();
-            SqlCommand cmd = new SqlCommand("Update Urun set miktari = miktari+ '" + int.Parse(MiktarTxt.Text) + "' where barkodno = '" + BarkodNoTxt.Text + "'", connection);
-            cmd.ExecuteNonQuery();
-            connection.Close();
+            //connection.Open();
+            //SqlCommand cmd = new SqlCommand("Update Urun set miktari = miktari+ '" + int.Parse(MiktarTxt.Text) + "' where barkodno = '" + BarkodNoTxt.Text + "'", connection);
+            //cmd.ExecuteNonQuery();
+            //connection.Close();
 
-            foreach (Control item in groupBox2.Controls)
-            {
-                if (item is System.Windows.Forms.TextBox)
-                {
-                    item.Text = "";
-                }
-            }
-            MessageBox.Show("Var Olan Ürüne Ekleme Yapıldı");
+            //foreach (Control item in groupBox2.Controls)
+            //{
+            //    if (item is System.Windows.Forms.TextBox)
+            //    {
+            //        item.Text = "";
+            //    }
+            //}
+            //MessageBox.Show("Var Olan Ürüne Ekleme Yapıldı");
         }
     }
 }
--- BookStock/frmUrunListeleme.Designer.cs
+++ BookStock/frmUrunListeleme.Designer.cs
@@ -34,8 +34,6 @@
             this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
-            this.label19 = new System.Windows.Forms.Label();
-            this.label20 = new System.Windows.Forms.Label();
             this.label21 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
@@ -48,17 +46,10 @@
             this.MiktarTxt = new System.Windows.Forms.TextBox();
             this.AlisFiyatTxt = new System.Windows.Forms.TextBox();
             this.BarkodNoTxt = new System.Windows.Forms.TextBox();
-            this.KategoriTxt = new System.Windows.Forms.TextBox();
             this.UrunAdiTxt = new System.Windows.Forms.TextBox();
-            this.YazarTxt = new System.Windows.Forms.TextBox();
             this.btnSil = new System.Windows.Forms.Button();
             this.txtBarkodNoAra = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboKategori = new System.Windows.Forms.ComboBox();
-            this.comboMarka = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnMarkaGüncelle = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,28 +57,28 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(358, 73);
+            this.dataGridView1.Location = new System.Drawing.Point(335, 73);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(732, 332);
+            this.dataGridView1.Size = new System.Drawing.Size(755, 332);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
             // 
             // btnGüncelle
             // 
-            this.btnGüncelle.Location = new System.Drawing.Point(235, 309);
+            this.btnGüncelle.Location = new System.Drawing.Point(137, 309);
             this.btnGüncelle.Name = "btnGüncelle";
-            this.btnGüncelle.Size = new System.Drawing.Size(94, 46);
+            this.btnGüncelle.Size = new System.Drawing.Size(192, 46);
             this.btnGüncelle.TabIndex = 14;
-            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.Text = "Ürünü Güncelle";
             this.btnGüncelle.UseVisualStyleBackColor = true;
             this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
             // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(27, 272);
+            this.label15.Location = new System.Drawing.Point(24, 256);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(72, 16);
             this.label15.TabIndex = 28;
@@ -96,7 +87,7 @@
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(35, 239);
+            this.label16.Location = new System.Drawing.Point(29, 209);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(64, 16);
             this.label16.TabIndex = 29;
@@ -105,7 +96,7 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(53, 206);
+            this.label17.Location = new System.Drawing.Point(47, 168);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(46, 16);
             this.label17.TabIndex = 30;
@@ -114,29 +105,11 @@
             // label18
             // 
             this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(41, 170);
+            this.label18.Location = new System.Drawing.Point(36, 114);
             this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(58, 16);
+            this.label18.Size = new System.Drawing.Size(57, 16);
             this.label18.TabIndex = 31;
-            this.label18.Text = "Ürün Adı";
-            // 
-            // label19
-            // 
-            this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(54, 138);
-            this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(42, 16);
-            this.label19.TabIndex = 32;
-            this.label19.Text = "Yazar";
-            // 
-            // label20
-            // 
-            this.label20.AutoSize = true;
-            this.label20.Location = new System.Drawing.Point(42, 106);
-            this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(57, 16);
-            this.label20.TabIndex = 33;
-            this.label20.Text = "Kategori";
+            this.label18.Text = "Stok Adı";
             // 
             // label21
             // 
@@ -151,7 +124,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(99, 90);
+            this.label8.Location = new System.Drawing.Point(99, 95);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(0, 16);
             this.label8.TabIndex = 26;
@@ -159,7 +132,7 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(99, 266);
+            this.label14.Location = new System.Drawing.Point(99, 279);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(0, 16);
             this.label14.TabIndex = 27;
@@ -167,7 +140,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(99, 234);
+            this.label13.Location = new System.Drawing.Point(99, 241);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(0, 16);
             this.label13.TabIndex = 25;
@@ -175,7 +148,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(99, 202);
+            this.label12.Location = new System.Drawing.Point(99, 203);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(0, 16);
             this.label12.TabIndex = 24;
@@ -183,7 +156,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(99, 186);
+            this.label11.Location = new System.Drawing.Point(99, 187);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(0, 16);
             this.label11.TabIndex = 23;
@@ -191,7 +164,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(99, 154);
+            this.label10.Location = new System.Drawing.Point(99, 149);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(0, 16);
             this.label10.TabIndex = 22;
@@ -199,67 +172,51 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(99, 122);
+            this.label9.Location = new System.Drawing.Point(99, 133);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(0, 16);
             this.label9.TabIndex = 21;
             // 
             // SatisFiyatTxt
             // 
-            this.SatisFiyatTxt.Location = new System.Drawing.Point(105, 266);
+            this.SatisFiyatTxt.Location = new System.Drawing.Point(99, 250);
             this.SatisFiyatTxt.Name = "SatisFiyatTxt";
             this.SatisFiyatTxt.Size = new System.Drawing.Size(224, 22);
             this.SatisFiyatTxt.TabIndex = 19;
             // 
             // MiktarTxt
             // 
-            this.MiktarTxt.Location = new System.Drawing.Point(105, 200);
+            this.MiktarTxt.Location = new System.Drawing.Point(99, 162);
             this.MiktarTxt.Name = "MiktarTxt";
             this.MiktarTxt.Size = new System.Drawing.Size(224, 22);
             this.MiktarTxt.TabIndex = 18;
             // 
             // AlisFiyatTxt
             // 
-            this.AlisFiyatTxt.Location = new System.Drawing.Point(105, 233);
+            this.AlisFiyatTxt.Location = new System.Drawing.Point(99, 203);
             this.AlisFiyatTxt.Name = "AlisFiyatTxt";
             this.AlisFiyatTxt.Size = new System.Drawing.Size(224, 22);
             this.AlisFiyatTxt.TabIndex = 17;
             // 
             // BarkodNoTxt
             // 
-            this.BarkodNoTxt.Location = new System.Drawing.Point(105, 73);
+            this.BarkodNoTxt.Location = new System.Drawing.Point(99, 73);
             this.BarkodNoTxt.Name = "BarkodNoTxt";
             this.BarkodNoTxt.Size = new System.Drawing.Size(224, 22);
             this.BarkodNoTxt.TabIndex = 16;
             // 
-            // KategoriTxt
-            // 
-            this.KategoriTxt.Location = new System.Drawing.Point(105, 101);
-            this.KategoriTxt.Name = "KategoriTxt";
-            this.KategoriTxt.ReadOnly = true;
-            this.KategoriTxt.Size = new System.Drawing.Size(224, 22);
-            this.KategoriTxt.TabIndex = 15;
-            // 
             // UrunAdiTxt
             // 
-            this.UrunAdiTxt.Location = new System.Drawing.Point(105, 167);
+            this.UrunAdiTxt.Location = new System.Drawing.Point(99, 111);
             this.UrunAdiTxt.Name = "UrunAdiTxt";
             this.UrunAdiTxt.Size = new System.Drawing.Size(224, 22);
             this.UrunAdiTxt.TabIndex = 20;
-            // 
-            // YazarTxt
-            // 
-            this.YazarTxt.Location = new System.Drawing.Point(105, 134);
-            this.YazarTxt.Name = "YazarTxt";
-            this.YazarTxt.ReadOnly = true;
-            this.YazarTxt.Size = new System.Drawing.Size(224, 22);
-            this.YazarTxt.TabIndex = 13;
             // 
             // btnSil
             // 
             this.btnSil.Location = new System.Drawing.Point(886, 423);
             this.btnSil.Name = "btnSil";
-            this.btnSil.Size = new System.Drawing.Size(125, 55);
+            this.btnSil.Size = new System.Drawing.Size(204, 55);
             this.btnSil.TabIndex = 35;
             this.btnSil.Text = "Sil";
             this.btnSil.UseVisualStyleBackColor = true;
@@ -283,72 +240,20 @@
             this.label1.Text = "BarkodNo ya Göre Ara";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // comboKategori
-            // 
-            this.comboKategori.FormattingEnabled = true;
-            this.comboKategori.Location = new System.Drawing.Point(488, 415);
-            this.comboKategori.Name = "comboKategori";
-            this.comboKategori.Size = new System.Drawing.Size(173, 24);
-            this.comboKategori.TabIndex = 38;
-            this.comboKategori.SelectedIndexChanged += new System.EventHandler(this.comboKategori_SelectedIndexChanged);
-            // 
-            // comboMarka
-            // 
-            this.comboMarka.FormattingEnabled = true;
-            this.comboMarka.Location = new System.Drawing.Point(488, 460);
-            this.comboMarka.Name = "comboMarka";
-            this.comboMarka.Size = new System.Drawing.Size(173, 24);
-            this.comboMarka.TabIndex = 39;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(394, 423);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 16);
-            this.label2.TabIndex = 33;
-            this.label2.Text = "Kategori";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(406, 455);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 16);
-            this.label3.TabIndex = 32;
-            this.label3.Text = "Yazar";
-            // 
-            // btnMarkaGüncelle
-            // 
-            this.btnMarkaGüncelle.Location = new System.Drawing.Point(713, 423);
-            this.btnMarkaGüncelle.Name = "btnMarkaGüncelle";
-            this.btnMarkaGüncelle.Size = new System.Drawing.Size(138, 55);
-            this.btnMarkaGüncelle.TabIndex = 14;
-            this.btnMarkaGüncelle.Text = "Kategori ve Yazar Güncelle";
-            this.btnMarkaGüncelle.UseVisualStyleBackColor = true;
-            this.btnMarkaGüncelle.Click += new System.EventHandler(this.btnMarkaGüncelle_Click);
-            // 
             // frmUrunListeleme
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(1102, 501);
-            this.Controls.Add(this.comboMarka);
-            this.Controls.Add(this.comboKategori);
+            this.ClientSize = new System.Drawing.Size(1102, 483);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtBarkodNoAra);
             this.Controls.Add(this.btnSil);
-            this.Controls.Add(this.btnMarkaGüncelle);
             this.Controls.Add(this.btnGüncelle);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.label17);
             this.Controls.Add(this.label18);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label19);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label20);
             this.Controls.Add(this.label21);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label14);
@@ -361,9 +266,7 @@
             this.Controls.Add(this.MiktarTxt);
             this.Controls.Add(this.AlisFiyatTxt);
             this.Controls.Add(this.BarkodNoTxt);
-            this.Controls.Add(this.KategoriTxt);
             this.Controls.Add(this.UrunAdiTxt);
-            this.Controls.Add(this.YazarTxt);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmUrunListeleme";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -383,8 +286,6 @@
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label18;
-        private System.Windows.Forms.Label label19;
-        private System.Windows.Forms.Label label20;
         private System.Windows.Forms.Label label21;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label14;
@@ -397,16 +298,9 @@
         private System.Windows.Forms.TextBox MiktarTxt;
         private System.Windows.Forms.TextBox AlisFiyatTxt;
         private System.Windows.Forms.TextBox BarkodNoTxt;
-        private System.Windows.Forms.TextBox KategoriTxt;
         private System.Windows.Forms.TextBox UrunAdiTxt;
-        private System.Windows.Forms.TextBox YazarTxt;
         private System.Windows.Forms.Button btnSil;
         private System.Windows.Forms.TextBox txtBarkodNoAra;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboKategori;
-        private System.Windows.Forms.ComboBox comboMarka;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnMarkaGüncelle;
     }
 }
