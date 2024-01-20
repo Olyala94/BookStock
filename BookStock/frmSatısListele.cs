86d71243d2adbc870fa78685f3daf5f540049282
olyadev <yusupowa9494@mail.ru>
2024-01-20 07:22:37 +0300

    Done

--- BookStock/frmSatısListele.cs
+++ BookStock/frmSatısListele.cs
@@ -20,6 +20,14 @@ namespace BookStock
             adapter.Fill(dataSet, "Satis");
             dataGridView1.DataSource = dataSet.Tables["Satis"];
 
+            // Sütun başlıklarını düzenli gözükmesi için
+            dataGridView1.Columns["barkodno"].HeaderText = "Barkod No";
+            dataGridView1.Columns["urunadi"].HeaderText = "Ürün Adı";
+            dataGridView1.Columns["miktari"].HeaderText = "Miktarı";
+            dataGridView1.Columns["satisfiyati"].HeaderText = "Satış Fiyatı";
+            dataGridView1.Columns["toplamfiyati"].HeaderText = "Toplam Fiyat";
+            dataGridView1.Columns["tarih"].HeaderText = "Tarih";
+
             connection.Close();
         }
         private void frmSatısListele_Load(object sender, EventArgs e)
--- BookStock/frmUrunListeleme.cs
+++ BookStock/frmUrunListeleme.cs
@@ -14,22 +14,16 @@ namespace BookStock
 
         SqlConnection connection = new SqlConnection("Data Source=DESKTOP-9E5P6SH;Initial Catalog=StockDb;Integrated Security=True;");
         DataSet dataSet = new DataSet();
-        private void KategoriGetir()
-        {
-            connection.Open();
-            SqlCommand cmd = new SqlCommand("select *from KategoriBilgileri", connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                //comboKategori.Items.Add(reader["kategori"].ToString());
-            }
-            connection.Close();
-        }
 
         private void frmUrunListeleme_Load(object sender, EventArgs e)
         {
             UrunListele();
-            KategoriGetir();
+            // DataGridView sütun başlıklarını güzelleştirmek için
+            dataGridView1.Columns["barkodno"].HeaderText = "Barkod No";
+            dataGridView1.Columns["urunadi"].HeaderText = "Ürün Adı";
+            dataGridView1.Columns["miktari"].HeaderText = "Miktarı";
+            dataGridView1.Columns["alisfiyati"].HeaderText = "Alış Fiyatı";
+            dataGridView1.Columns["satisfiyati"].HeaderText = "Satış Fiyatı";
         }
 
         private void UrunListele()
@@ -48,9 +42,7 @@ namespace BookStock
 
         private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            BarkodNoTxt.Text = dataGridView1.CurrentRow.Cells["barkodno"].Value.ToString();
-            //KategoriTxt.Text = dataGridView1.CurrentRow.Cells["kategori"].Value.ToString();
-            //YazarTxt.Text = dataGridView1.CurrentRow.Cells["marka"].Value.ToString();
+            BarkodNoTxt.Text = dataGridView1.CurrentRow.Cells["barkodno"].Value.ToString(); 
             UrunAdiTxt.Text = dataGridView1.CurrentRow.Cells["urunadi"].Value.ToString();
             MiktarTxt.Text = dataGridView1.CurrentRow.Cells["miktari"].Value.ToString();
             AlisFiyatTxt.Text = dataGridView1.CurrentRow.Cells["alisfiyati"].Value.ToString();
@@ -68,7 +60,7 @@ namespace BookStock
             cmd.Parameters.AddWithValue("@satisfiyati", double.Parse(SatisFiyatTxt.Text));
             cmd.ExecuteNonQuery();
             connection.Close();
-            dataSet.Tables["Urun"].Clear();//önce tabloyu temizle, sonra tekrar listele 
+            dataSet.Tables["Urun"].Clear(); //önce tabloyu temizle, sonra tekrar listele....
             UrunListele();
             MessageBox.Show("Güncelleme Başarı bir şekilde Yapıldı");
             foreach (Control item in this.Controls)
@@ -82,46 +74,11 @@ namespace BookStock
 
         private void btnMarkaGüncelle_Click(object sender, EventArgs e)
         {
-            //if (BarkodNoTxt.Text != "")
-            //{
-            //    connection.Open();
-            //    SqlCommand cmd = new SqlCommand("Update Urun set kategori=@kategori, marka=@marka where barkodno=@barkodno", connection);
-            //    cmd.Parameters.AddWithValue("@barkodno", BarkodNoTxt.Text);
-            //    cmd.Parameters.AddWithValue("@kategori", comboKategori.Text);
-            //    cmd.Parameters.AddWithValue("@marka", comboMarka.Text);
-
-            //    cmd.ExecuteNonQuery();
-            //    connection.Close();
-            //    MessageBox.Show("Güncelleme Başarı bir şekilde Yapıldı");
-            //    dataSet.Tables["Urun"].Clear();
-            //    UrunListele();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("BarkodNo yazılı değil");
-            //}
-
-            //foreach (Control item in this.Controls)
-            //{
-            //    if (item is ComboBox)
-            //    {
-            //        ((ComboBox)item).Text = "";
-            //    }
-            //}
+            
         }
 
         private void comboKategori_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ////comboMarka.Items.Clear();
-            ////comboMarka.Text = "";
-            //connection.Open();
-            //SqlCommand cmd = new SqlCommand("select * from MarkaBilgileri where kategori ='" + /*comboKategori.SelectedItem + "'", connection);*/
-            //SqlDataReader reader = cmd.ExecuteReader();
-            //while (reader.Read())
-            //{
-            //    comboKategori.Items.Add(reader["marka"].ToString());
-            //}
-            //connection.Close();
+        {          
         }
 
         private void btnSil_Click(object sender, EventArgs e)
